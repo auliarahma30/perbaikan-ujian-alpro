1d45a5c6d41dd3774d9e3e841a38530d5a126c2d
auliarahma30 <rahma30012006@gmail.com>
2024-12-18 09:03:29 +0700

    Add files via upload

---
+++ jumlah_bilanga.py
@@ -0,0 +1,12 @@
+n = int(input("Masukkan nilai n: "))
+       
+total = 0
+i = 1
+
+while 1 <= n : 
+    total += i 
+    i =+ 1
+    
+print("Jumlah bilangan dari 1 hingga", n, "adalah:", total)
+
+
---
+++ luas_lingkaran.py
@@ -0,0 +1,8 @@
+import math 
+r = float(input("Masukkan jari-jari lingkaran: "))
+
+luas = math.pi *r *r
+print(f"luas lingkaran dengan jari-jari, {r}, adalah:, {luas}")
+
+         
+
